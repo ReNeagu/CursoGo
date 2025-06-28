675d4cfb78f26ac48a5a7f5ea8ce570a9accddb8
Renato Neagu <neagu.re@gmail.com>
2025-06-28 14:31:15 -0300

    Teste Msg de Alteração

--- 11 - Estruturas de Controle/estrutura-de-controle.go
+++ 11 - Estruturas de Controle/estrutura-de-controle.go
@@ -5,12 +5,18 @@ import "fmt"
 func main() {
 	fmt.Println("Estruturas de Controle")
 
-	numero := 10
+	numero := 20
 
 	if numero > 15 {
 		fmt.Println("Maior do que 15")
 	} else {
 		fmt.Println("Menor do que 15")
+	}
+
+	if outroNumero := numero; outroNumero > 30 {
+		fmt.Println("Número é maior do que 0")
+	} else {
+		fmt.Println("Numero é menor que 30")
 	}
 
 }
